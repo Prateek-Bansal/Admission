b74b4edd9bb68d150b979c5ad9b91c5bea46750d
Prateek-Bansal <prateekbansal1827@gmail.com>
2018-07-19 11:06:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Context
+This dataset is created for prediction of Admissions for higher studies from the students' perspective.
+
+# Content
+The dataset contains several parameters which are considered important during the application for Masters Programs. The parameters included are :
+1. GRE Scores ( out of 340 ) 
+2. TOEFL Scores ( out of 120 )
+3. University Rating ( out of 5 ) 
+4. Statement of Purpose and Letter of Recommendation Strength ( out of 5 ) 
+5. Undergraduate GPA ( out of 10 ) 
+6. Research Experience ( either 0 or 1 ) 
+7. Chance of Admit ( ranging from 0 to 1 )
+
+# Acknowledgements
+This dataset is inspired by the UCLA Graduate Dataset. The test scores and GPA are in the older format. The dataset is owned by Mohan S Acharya.
+
+# Inspiration
+This dataset was built with the purpose of helping students in shortlisting universities with their profiles.
+The predicted output gives them a fair idea about their chances for a particular university.
+
+# Accuracy Comparison of various models:
+Decision Tree Classifier :88.75%
+K Nearest Neighbours :91.25%
+kernel Svm :92.50%
+Logistic Regression :93.75%
+Naive Bayes :91.25%
+Random Forest Classifier :91.25%
+SVM : 91.25%
---
+++ decision tree classifier.py
@@ -0,0 +1,64 @@
+# decision tree classification
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataSet
+dataset=pd.read_csv('Admission_Predict.csv')
+X=dataset.iloc[:,1:8].values #to make it matrix independent->matrix dependent->array
+Y=dataset.iloc[:,8].values
+
+for i in range(0,400):
+    if(Y[i]>=0.5):
+        Y[i]=1
+    else:
+        Y[i]=0
+
+#Splitting the dataset into the training set and test set
+from sklearn.cross_validation import train_test_split
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2,random_state=0)
+
+#Feature Scaling 
+from sklearn.preprocessing import StandardScaler
+sc_X=StandardScaler()
+X_train=sc_X.fit_transform(X_train)
+X_test=sc_X.transform(X_test)
+
+# Applying PCA
+from sklearn.decomposition import PCA
+pca = PCA(n_components = 2)
+X_train = pca.fit_transform(X_train)
+X_test = pca.transform(X_test)
+explained_variance = pca.explained_variance_ratio_
+
+#Fitting decision tree classification regression to training set
+from sklearn.tree import DecisionTreeClassifier
+classifier=DecisionTreeClassifier(criterion='entropy',random_state=0) 
+classifier.fit(X_train,Y_train)
+
+#predicting test set results
+Y_pred=classifier.predict(X_test)
+
+#making the confusion matrix
+from sklearn.metrics import confusion_matrix
+cm=confusion_matrix(Y_test,Y_pred)
+
+#visualising test set results
+from matplotlib.colors import ListedColormap
+X_set,Y_set=X_test,Y_test
+X1,X2=np.meshgrid(np.arange(start=X_set[:,0].min()-1,stop=X_set[:,0].max()+1,step=0.01),
+                  np.arange(start=X_set[:,1].min()-1,stop=X_set[:,1].max()+1,step=0.01))
+plt.contourf(X1,X2,classifier.predict(np.array([X1.ravel(),X2.ravel()]).T).reshape(X1.shape),alpha=0.75,cmap=ListedColormap(('red','green')))
+plt.xlim(X1.min(),X1.max())
+plt.ylim(X2.min(),X2.max())
+for i,j in enumerate(np.unique(Y_set)):
+    plt.scatter(X_set[Y_set==j,0],X_set[Y_set==j,1],
+                c=ListedColormap(('red','green'))(i),label=j)
+                      
+plt.title('Decision Tree Classifier(Test set)')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend()
+plt.show()
